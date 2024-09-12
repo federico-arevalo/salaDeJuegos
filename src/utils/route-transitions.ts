0f3509939e10724ce08244ac0bc5c2b5eb546145
federico-arevalo <arevalofd98@gmail.com>
2024-09-12 00:02:53 -0300

    fix animations

--- src/utils/route-transitions.ts
+++ src/utils/route-transitions.ts
@@ -11,9 +11,13 @@ export const routeTransition = trigger('routeTransition', [
     query(':enter', [style({ opacity: 0, scale: 0.9 })], {
       optional: true,
     }),
-    query(':leave', [animate('0.2s', style({ opacity: 0, scale: 0.9 }))], {
-      optional: true,
-    }),
+    query(
+      ':leave',
+      [animate('0.5s', style({ opacity: 0, scale: 0.9, display: 'none' }))],
+      {
+        optional: true,
+      }
+    ),
     query(':enter', [animate('0.2s', style({ opacity: 1, scale: 1 }))], {
       optional: true,
     }),
