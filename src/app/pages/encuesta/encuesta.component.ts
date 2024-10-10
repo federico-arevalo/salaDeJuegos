080d56d843b916980a5d568792cb2962d5dff955
federico-arevalo <arevalofd98@gmail.com>
2024-10-09 23:28:02 -0300

    complete encuesta
    completed encuesta
    added encuesta service

--- src/app/pages/encuesta/encuesta.component.ts
+++ src/app/pages/encuesta/encuesta.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-encuesta',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './encuesta.component.html',
   styleUrl: './encuesta.component.scss',
 })
 export class EncuestaComponent {
+  encuestaForm = new FormGroup({
+    nombre: new FormControl(
+      { value: this.currentUser, disabled: true },
+      Validators.required
+    ),
+    edad: new FormControl('', Validators.required),
+    genero: new FormControl('', Validators.required),
+    satisfaction: new FormControl('', Validators.required),
+    juegosfav: new FormControl('', Validators.required),
+    msj: new FormControl(''),
+  });
+
   public get currentUser(): string {
     return JSON.parse(localStorage.getItem('user')!).email;
+  }
+
+  sendEncuesta() {
+    console.log(this.encuestaForm.value);
+    console.log(this.encuestaForm.valid);
   }
 }
